3183711f2c51b669e688309db60d60a8d19ec202
Michael Barton <mail@michaelbarton.me.uk>
2015-06-29 14:17:51 +0100

    Extract SDB domain to function
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- src/util.py
+++ src/util.py
@@ -6,3 +6,10 @@ def environment_var(name):
     else:
       sys.stderr.write("Environment variable not set: {}\n".format(name))
       exit(1)
+
+def sdb_domain():
+    import boto.sdb
+    conn = boto.sdb.connect_to_region('us-west-1',
+      aws_access_key_id     = environment_var('AWS_ACCESS_KEY'),
+      aws_secret_access_key = environment_var('AWS_SECRET_KEY'))
+    return conn.get_domain(environment_var('AWS_SIMPLEDB_NAME'))
