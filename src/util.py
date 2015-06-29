fe1a22643ebea73bddb54d9a340de0c5a5dda768
Michael Barton <mail@michaelbarton.me.uk>
2015-06-29 15:24:34 +0100

    Function to convert simpledb data in pandas dataframe
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- src/util.py
+++ src/util.py
@@ -13,3 +13,7 @@ def sdb_domain():
       aws_access_key_id     = environment_var('AWS_ACCESS_KEY'),
       aws_secret_access_key = environment_var('AWS_SECRET_KEY'))
     return conn.get_domain(environment_var('AWS_SIMPLEDB_NAME'))
+
+def fetch_all_metrics():
+    return sdb_domain().select(
+        'select * from `{}`'.format(environment_var('AWS_SIMPLEDB_NAME')))
